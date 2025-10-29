9f08cf437d902598ca2e3eace67cbd91d508132f
999rajat99-debug <999rajat99@gmail.com>
2025-10-29 08:04:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,18 @@ const PORT = process.env.PORT || 10000;
 // =============================
 // Firebase Setup
 // =============================
-const serviceAccount = require("./serviceAccountKey.json");
-
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://newsignup-default-rtdb.firebaseio.com/" // ✅ your Firebase URL
