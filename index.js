865b48a8b1bac1df7cacb2de46df473f1a365ea1
999rajat99-debug <999rajat99@gmail.com>
2025-10-30 15:54:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const serviceAccount = {
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+    databaseURL: "https://newsiginup-default-rtdb.firebaseio.com/"
   });
 }
 
